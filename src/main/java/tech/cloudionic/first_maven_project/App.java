a9337499e604762170b777c542bffd85553f88b3
Rohanathan S <41891930+rohanathan@users.noreply.github.com>
2024-01-20 23:11:17 +0530

    CN2.0

--- src/main/java/tech/cloudionic/first_maven_project/App.java
+++ src/main/java/tech/cloudionic/first_maven_project/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to CN World!" );
+        System.out.println( "Welcome to CN World2.0!" );
     }
 }
